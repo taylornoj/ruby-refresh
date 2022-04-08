afe777e3bdb26309e56a5503ddbb826d084d8ee9
Taylor Noj <taylornoj@gmail.com>
2022-04-08 14:04:04 +0000

    add exponentiation

--- script.rb
+++ script.rb
@@ -5,3 +5,8 @@ my_string = "Ruby"
 puts my_num
 puts my_boolean
 puts my_string
+
+# exponentiation (**)
+# raises one number (the base) to the power of the other number (exponent)
+expo = 2**3
+puts expo
