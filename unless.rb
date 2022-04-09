bcb7663cb08196a5fe85e4133e29ef0f5edddc9c
Taylor Noj <taylornoj@gmail.com>
2022-04-09 15:14:53 +0000

    add unless example

---
+++ unless.rb
@@ -0,0 +1,12 @@
+hungry = false
+
+unless hungry
+  puts "I'm writing Ruby programs!"
+else
+  puts "Time to eat!"
+end
+
+=begin
+you don't want to eat unless you're hungry
+while you're not hungry, write programs, but if you are, you eat
+=end
