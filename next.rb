155f0552742b600d8ab1880c0f02c0a7484660de
Taylor Noj <taylornoj@gmail.com>
2022-04-11 19:37:28 +0000

    add next example

---
+++ next.rb
@@ -0,0 +1,8 @@
+# can be used to skip over certain steps in the loop
+
+# loop through range of 1 through 5 assigning each number to i
+for i in 1..5
+  # if remainder of 1/2 == 0, we go to enxt iteration of the loop
+  next if i % 2 == 0
+  print i
+end
