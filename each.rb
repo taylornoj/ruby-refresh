051da0e31e72e0244c87b78440b351b909414fc7
Taylor Noj <taylornoj@gmail.com>
2022-04-11 20:19:43 +0000

    add notes and examples of each iterator

---
+++ each.rb
@@ -0,0 +1,16 @@
+=begin
+ 
+the .each iterator: 
+can apply an expression to each element of an object, one at a time
+
+object.each { |item| 
+  # Do something 
+}
+
+or
+
+object.each do |item| 
+  # Do something 
+end
+
+=end
