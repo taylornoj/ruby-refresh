9e8a3071695a99eeaf2ef2ce47efa8a2dafd43de
Taylor Noj <taylornoj@gmail.com>
2022-04-11 18:46:30 +0000

    add notes on iterators

---
+++ iterator.rb
@@ -0,0 +1,10 @@
+=begin
+ a ruby method that repeatedly invokes a block of code
+
+an example is the loop method:
+  loop { print "Hello, world!" }
+however this is infinite. the { } are interchangeable with keywords 'do'
+and 'end' (to open and close the block)
+
+=end
+
