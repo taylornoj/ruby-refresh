f08ef7184f5b8c1d67a6d9d48ccc862dbd81aa6a
Taylor Noj <taylornoj@gmail.com>
2022-04-12 17:00:55 +0000

    add notes re split method

--- redact.rb
+++ redact.rb
@@ -3,6 +3,9 @@ text = gets.chomp
 puts "Word to redact: "
 redact = gets.chomp
 
+# split method - takes in a string and returns an array
+# if we pass it some text in parentheses, split will divide the string 
+# whereever it sees that bit of text (a delimiter)
 words = text.split(" ")
 
 words.each do |word|
