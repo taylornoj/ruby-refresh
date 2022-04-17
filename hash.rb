3d57da12ea1ca56d300992d1f814ebdc11105863
Taylor Noj <taylornoj@gmail.com>
2022-04-17 21:55:39 +0000

    add key value pair in new.hash

--- hash.rb
+++ hash.rb
@@ -5,3 +5,7 @@ Hash literal is within { key => value }
 Hash.new is the samething just different syntax
 ex. my_hash = Hash.new
 =end
+
+pets = Hash.new
+pets["Ellie"] = "dog"
+# adds the key "Ellie" with value "dog"
