85662bc024bd1d75749b9b5fe98122c772326f55
Taylor Noj <taylornoj@gmail.com>
2022-04-21 21:53:44 +0000

    add splat example

--- splat.rb
+++ splat.rb
@@ -13,3 +13,9 @@ Solution is splat arguments - preceded by a * which tells the program that the
 method can receive one or more arguments
 
 =end
+
+def what_up(greeting, *friends)
+  friends.each { |friend| puts "#{greeting}, #{friend}!" }
+end
+
+what_up("What up", "Ian", "Zoe", "Zenas", "Eleanor")
