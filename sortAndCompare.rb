a84846e7fed628391cb383c9f496a46d0b610dc6
Taylor Noj <taylornoj@gmail.com>
2022-04-22 16:35:42 +0000

    example or sort in ascending and descending order

---
+++ sortAndCompare.rb
@@ -0,0 +1,14 @@
+# sort books by title from Z - A/descending order
+
+# Ruby's sort method only works for A - Z/ascending order
+# sort books in descending order:
+
+books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+
+# To sort our books in ascending order, in-place
+books.sort! { |firstBook, secondBook| firstBook <=> secondBook }
+
+# Sort your books in descending order, in-place below
+books.sort! { |firstBook, secondBook| secondBook <=> firstBook }
+
+print books
