d57adeb78208b3f51360d72de06c277e8207ad76
Taylor Noj <taylornoj@gmail.com>
2022-04-22 16:19:28 +0000

    add info re combined comparison

---
+++ combinedComparison.rb
@@ -0,0 +1,11 @@
+# combined comparison operator <=>
+
+# returns 0 if the first operand (item to be compared) equals the second
+# returns 1 if the first operand is greater than the second
+# returns -1 if the first operand is less than the second
+
+book_1 = "A Wrinkle in Time"
+
+book_2 = "A Brief History of Time"
+
+puts book_1 <=> book_2     # prints 1
