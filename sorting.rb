6aae2f707a760159339e674624d9dec809641d00
Taylor Noj <taylornoj@gmail.com>
2022-04-22 15:43:34 +0000

    add sort example alphabetically

--- sorting.rb
+++ sorting.rb
@@ -6,3 +6,10 @@ my_array = [3, 4, 8, 7, 1, 6, 5, 9, 2]
 # my_array should then equal [1, 2, 3, 4, 5, 6, 7, 8, 9].
 
 print my_array.sort!
+
+
+
+books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+
+# will sort alphabetically 
+print books.sort!
