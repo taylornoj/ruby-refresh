91cf5b584b7eb56bacce8ab263f6c3bc39cf4480
Taylor Noj <taylornoj@gmail.com>
2022-04-22 15:09:05 +0000

    frist example or sort! method

---
+++ sorting.rb
@@ -0,0 +1,8 @@
+# Ruby's built in sorting library
+
+my_array = [3, 4, 8, 7, 1, 6, 5, 9, 2]
+
+# Call the sort! method on my_array below.
+# my_array should then equal [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+print my_array.sort!
